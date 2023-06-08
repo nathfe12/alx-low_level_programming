abd17691de421cda9d6424c9f6a22ad56bc79fa4
nathfe12 <nathfe90@gmail.com>
2023-06-07 17:53:47 -0700

    Update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int _sqrt_recursion_wrapper(int n, int min, int max)
 	int guess, guess_squared;
 
 	guess = (min + max) / 2;
-i	guess_squared = guess * guess;
+	guess_squared = guess * guess;
 
 	if (guess_squared == n)
 		return (guess);
