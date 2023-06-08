2aee4f9c8636f968762fcc0b31c5524c8ed13d49
nathfe12 <nathfe90@gmail.com>
2023-06-07 17:17:45 -0700

    Update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -18,7 +18,7 @@ int is_palindrome(char *s)
 
 /**
  * _strlen_recursion - returns the length of a string
- *@s: string to calculate the length of 
+ *@s: string to calculate the length of
  *
  i* Return: length of the string
  */
