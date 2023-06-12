cf046cbb8067ff2d0d6d3a8a947d354bf9863c49
nathfe12 <nathfe90@gmail.com>
2023-06-12 14:35:21 -0700

    Update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -8,9 +8,13 @@ void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] >= 0; i++)
 	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
 		_putchar(str[i]);
 	}
-	_putchar('\n');
 }
