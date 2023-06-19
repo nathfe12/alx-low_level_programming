1e71e90b42a861bc0aca4c407f1cb27345ba38ee
nathfe12 <nathfe90@gmail.com>
2023-06-19 16:20:54 -0700

    Update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,7 +4,7 @@
 void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest,int dest_index);
 int check_for_digits(char **av);
-voud init(char *str, int l);
+void init(char *str, int l);
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
