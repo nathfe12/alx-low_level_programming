0322ad5c885c5f8e1a8289ee58f741aedb1676a5
nathfe12 <nathfe90@gmail.com>
2023-06-25 10:59:20 -0700

    Update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,4 +16,25 @@ int **alloc_grid(int width, int height)
 	if (len <= 0)
 		return (NULL);
 
-	array = (int **)
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(array[i]);
+			free(array);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i , height; i++)
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+
+	return (array);
+}
