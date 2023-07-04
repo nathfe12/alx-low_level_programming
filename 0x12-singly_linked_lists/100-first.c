8a9aeed19919061deeac94f03837dbb600438055
nathfe12 <nathfe90@gmail.com>
2023-07-04 02:22:02 -0700

    DUpdate

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  * Return: nothing
  */
 
-void myStartupFun(void) __attribute__ ((constructor));
+void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore my house upon my back!\n");
