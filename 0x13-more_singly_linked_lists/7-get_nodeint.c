af8f02055c973b6b6db56e10cdee7d7cc623ab5d
nathfe12 <nathfe90@gmail.com>
2023-07-06 13:35:14 -0700

    Update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,15 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
-	listint_t *temp = head;
-	
-	while (temp && i < index)
-	{
-		temp = temp->next;
-		i++;
-	}
+	unsigned int i;
 
-	return (temp ? temp : NULL)
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+		if (head == NULL)
+		       return (NULL);
+	}
+	return (head);
 }
