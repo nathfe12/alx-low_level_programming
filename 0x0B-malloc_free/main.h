37de2f0b130b00f457faf60e9c8b49aedeec01c0
nathfe12 <nathfe90@gmail.com>
2023-08-07 16:10:40 -0700

    Update

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-char *crear=te_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
