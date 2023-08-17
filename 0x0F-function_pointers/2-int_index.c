4535b1deea270c7ffbce00f908394e77ba6b3826
nathfe12 <nathfe90@gmail.com>
2023-08-17 13:14:51 -0700

    Update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "function_pointerss.h"
+#include "function_pointers.h"
 /**
  * int_index - return index place if comparison = true, else - 1
  * @array: array
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i])
+		if (cmp(array[i]))
 			return (i);
 	}
 	return (-1);
